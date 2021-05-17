816b1e68b5de7b64045ef45c021239344a472f09
Szymon Nowak <szimek@gmail.com>
2021-05-17 15:53:34 +0200

    Exercise 10.2 - Schlick Approximation

--- src/materials/Dielectric.ts
+++ src/materials/Dielectric.ts
@@ -1,22 +1,30 @@
 import { Color } from '../utils/color';
 import { HitRecord } from '../utils/HitRecord';
 import { Material, Scattered } from '../utils/Material';
+import { random } from '../utils/random';
 import { Ray } from '../utils/Ray';
 import { Vec3 } from '../utils/Vec3';
 
 export class Dielectric extends Material {
-  refractionIndex: number;
+  refractiveIndex: number;
 
-  constructor(refractionIndex: number) {
+  // Use Schlick's approximation for reflectance.
+  static reflectance(cosine: number, refractiveIndex: number): number {
+    const r0 = (1 - refractiveIndex) / (1 + refractiveIndex);
+    const r0Squared = r0 * r0;
+    return r0Squared + (1 - r0Squared) * (1 - cosine) ** 5;
+  }
+
+  constructor(refractiveIndex: number) {
     super();
-    this.refractionIndex = refractionIndex;
+    this.refractiveIndex = refractiveIndex;
   }
 
   scatter(hit: HitRecord, ray: Ray): Scattered | null {
     const attenuation = new Color(1.0, 1.0, 1.0);
     const refractionRatio = hit.frontFace
-      ? 1.0 / this.refractionIndex
-      : this.refractionIndex;
+      ? 1.0 / this.refractiveIndex
+      : this.refractiveIndex;
 
     const unitDirection = ray.direction.unit();
     const cosTheta = Math.min(unitDirection.negate().dot(hit.normal), 1.0);
@@ -24,7 +32,10 @@ export class Dielectric extends Material {
     const canRefract = refractionRatio * sinTheta <= 1.0;
     let direction: Vec3;
 
-    if (canRefract) {
+    if (
+      canRefract ||
+      Dielectric.reflectance(cosTheta, refractionRatio) <= random()
+    ) {
       direction = unitDirection.refract(hit.normal, refractionRatio);
     } else {
       direction = unitDirection.reflect(hit.normal);
